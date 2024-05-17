6ffb3c97a1d218e7c58575047034e29e841bb96d
Ari Wahidin <ari.wahidin@PYIDJKT.local>
2024-05-17 08:12:12 +0700

    work shcedule : fixing download example

--- application/views/work_schedule/index.php
+++ application/views/work_schedule/index.php
@@ -189,9 +189,9 @@
         $('#btnDownloadForm').on('click', downloadExcelForm)
 
         function downloadExcelForm() {
-            // startLoading();
+            startLoading();
             setTimeout(async function() {
-                    // stopLoading();
+                    stopLoading();
                     // let checker = $('#sChecker').val().trim();
                     // let startDate = $('#sStartDate').val().trim();
                     // let endDate = $('#sEndDate').val().trim();
@@ -215,7 +215,7 @@
                     var workbook = new ExcelJS.Workbook();
                     var sheet1 = workbook.addWorksheet('Form Schedule');
                     var sheet2 = workbook.addWorksheet('Master User');
-                    var sheet3 = workbook.addWorksheet('Example');
+                    // var sheet3 = workbook.addWorksheet('Example');
 
 
                     sheet1.addRow(headers).eachCell(function(row, rowNumber) {
@@ -238,15 +238,15 @@
                         };
                     });
 
-                    sheet3.addRow(headers_example).eachCell(function(row, rowNumber) {
-                        row.fill = {
-                            type: 'pattern',
-                            pattern: 'solid',
-                            fgColor: {
-                                argb: 'FFFF00'
-                            }
-                        };
-                    });
+                    // sheet3.addRow(headers_example).eachCell(function(row, rowNumber) {
+                    //     row.fill = {
+                    //         type: 'pattern',
+                    //         pattern: 'solid',
+                    //         fgColor: {
+                    //             argb: 'FFFF00'
+                    //         }
+                    //     };
+                    // });
 
                     // Menentukan lebar kolom berdasarkan isi
                     // sheet1.columns.forEach(function(column) {
@@ -310,24 +310,24 @@
                         });
                     });
 
-                    sheet3.eachRow(function(row) {
-                        row.eachCell(function(cell) {
-                            cell.border = {
-                                top: {
-                                    style: 'thin'
-                                },
-                                left: {
-                                    style: 'thin'
-                                },
-                                bottom: {
-                                    style: 'thin'
-                                },
-                                right: {
-                                    style: 'thin'
-                                }
-                            };
-                        });
-                    });
+                    // sheet3.eachRow(function(row) {
+                    //     row.eachCell(function(cell) {
+                    //         cell.border = {
+                    //             top: {
+                    //                 style: 'thin'
+                    //             },
+                    //             left: {
+                    //                 style: 'thin'
+                    //             },
+                    //             bottom: {
+                    //                 style: 'thin'
+                    //             },
+                    //             right: {
+                    //                 style: 'thin'
+                    //             }
+                    //         };
+                    //     });
+                    // });
 
                     // dataAct.header.forEach(function(row, ) {
                     //     var rowData = headers.map(function(header) {
@@ -406,47 +406,47 @@
                         });
                     });
 
-                    dataAct.example.forEach(function(row, ) {
+                    // dataAct.example.forEach(function(row, ) {
 
 
-                        var rowData = headers_example.map(function(header) {
-                            return row[header];
-                        });
+                    //     var rowData = headers_example.map(function(header) {
+                    //         return row[header];
+                    //     });
 
 
-                        sheet3.addRow(rowData);
-                        // Menentukan lebar kolom berdasarkan isi
-                        sheet3.columns.forEach(function(column) {
-                            var maxLength = 0;
-                            column.eachCell(function(cell) {
-                                var columnLength = cell.value ? cell.value.toString().length : 10;
-                                if (columnLength > maxLength) {
-                                    maxLength = columnLength;
-                                }
-                            });
-                            column.width = maxLength < 10 ? 10 : maxLength;
-                        });
+                    //     sheet3.addRow(rowData);
+                    //     // Menentukan lebar kolom berdasarkan isi
+                    //     sheet3.columns.forEach(function(column) {
+                    //         var maxLength = 0;
+                    //         column.eachCell(function(cell) {
+                    //             var columnLength = cell.value ? cell.value.toString().length : 10;
+                    //             if (columnLength > maxLength) {
+                    //                 maxLength = columnLength;
+                    //             }
+                    //         });
+                    //         column.width = maxLength < 10 ? 10 : maxLength;
+                    //     });
 
-                        // Menambahkan border ke seluruh tabel
-                        sheet3.eachRow(function(row) {
-                            row.eachCell(function(cell) {
-                                cell.border = {
-                                    top: {
-                                        style: 'thin'
-                                    },
-                                    left: {
-                                        style: 'thin'
-                                    },
-                                    bottom: {
-                                        style: 'thin'
-                                    },
-                                    right: {
-                                        style: 'thin'
-                                    }
-                                };
-                            });
-                        });
-                    });
+                    //     // Menambahkan border ke seluruh tabel
+                    //     sheet3.eachRow(function(row) {
+                    //         row.eachCell(function(cell) {
+                    //             cell.border = {
+                    //                 top: {
+                    //                     style: 'thin'
+                    //                 },
+                    //                 left: {
+                    //                     style: 'thin'
+                    //                 },
+                    //                 bottom: {
+                    //                     style: 'thin'
+                    //                 },
+                    //                 right: {
+                    //                     style: 'thin'
+                    //                 }
+                    //             };
+                    //         });
+                    //     });
+                    // });
 
                     workbook.xlsx.writeBuffer().then(function(buffer) {
                         var blob = new Blob([buffer], {
