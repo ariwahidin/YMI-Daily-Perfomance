defceaffd08ff8704c484a27828b95c0032504c9
Ari Wahidin <ari.wahidin@PYIDJKT.local>
2024-06-05 16:58:46 +0700

    dashboard : create man power plan and active on inbound summary card - rev

--- application/views/dashboard/table_inbound.php
+++ application/views/dashboard/table_inbound.php
@@ -16,12 +16,19 @@
         $no = 1;
         foreach ($inbound->result() as $data) {
             $duration_unload = countDuration($data->start_unload, $data->stop_unload);
+
             $duration_checking = countDuration($data->start_checking, $data->stop_checking);
             $duration_putaway = countDuration($data->start_putaway, $data->stop_putaway);
+
+            // var_dump($duration_unload);
+            // var_dump($duration_checking);
+            // var_dump($duration_putaway);
+
             $minute_unload = ($duration_unload != '') ? roundMinutes($duration_unload) : (($data->start_unload != null && $data->stop_unload == null) ? 'proccesing' : '');
             $minute_checking = ($duration_checking != '') ? roundMinutes($duration_checking) : (($data->start_checking != null && $data->stop_checking == null) ? 'proccesing' : '');
             $minute_putaway = ($duration_putaway != '') ? roundMinutes($duration_putaway) : (($data->start_putaway != null && $data->stop_putaway == null) ? 'proccesing' : '');
-            $status = ($minute_unload != 'processing' && $minute_checking != 'processing' && $minute_putaway != '' && $minute_unload != '' && $minute_checking != '' && $minute_putaway != '') ? 'DONE' : '';
+            // $status = ($minute_unload != 'processing' && $minute_checking != 'processing' && $minute_putaway != 'processing' && $minute_unload != '' && $minute_checking != '' && $minute_putaway != '') ? 'DONE' : '';
+            $status = ($duration_unload != '' && $duration_checking != '' && $duration_putaway != '') ? 'DONE' : '';
 
         ?>
 
