c0ec1ec7d3eba1e823f4e070cbb7eb12935a182d
Ari Wahidin <ari.wahidin@PYIDJKT.local>
2024-08-13 16:47:17 +0700

    fix direct isLogin

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -12,7 +12,7 @@ function is_logged_in()
 {
     $CI = &get_instance();
     if ($CI->session->userdata('user_data')) {
-        redirect(base_url('dashboard'));
+        redirect(base_url('dashboard/index'));
     }
 }
 
